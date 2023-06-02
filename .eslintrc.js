4a0cf1f71e507c529f0811549a7ec2661073cabd
KittyPigeon <1609401009@qq.com>
2023-06-02 15:45:18 +0800

    。。。。

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        // "indent": [2, 4],//缩进风格
-        "semi": ["error", "always"],
-        //"no-console": 2,//禁止使用console
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    // indent: [2, 4], //缩进风格
+    semi: ["error", "always"],
+    //"no-console": 2,//禁止使用console
+  },
 };
